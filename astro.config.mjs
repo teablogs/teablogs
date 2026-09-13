f3e3daf89801f648d3fa01209d53718828e2efd5
teablogs <yogeerama@gmail.com>
2026-09-14 01:22:52 +0530

    Add GitHUb Pages to Deployment- II

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://teablogs.github.io',
-	base: '/teablogs'
+	base: '/teablogs',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
